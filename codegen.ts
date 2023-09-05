761a682568fcec766aa8bd42a40f0f7d0e658edf
Patel Swapnil <patelswapnil1004@gmail.com>
2023-09-05 16:28:59 +0530

    fix : deployment error

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "https://graphqlserver-production.up.railway.app/",
+  schema: "https://catstronauts-graphql-server-production-0cef.up.railway.app",
   documents: ["src/**/*.tsx"],
   generates: {
     "./src/__generated__/": {
